b34ef11db24ecc1ff92b9e2d417af767c40d97c5
SiddheshDekhane <161200606+SiddheshDekhane@users.noreply.github.com>
2024-10-01 21:42:25 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-export const API_END_POINT = "https://backendnetfilx.onrender.com";
+export const API_END_POINT = "https://backendnetfilx.onrender.com/api/v1/user";
 
 export const options = {
     method: 'GET',
